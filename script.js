92c81861bc55e0308304f5fea9ef137ae02c8496
ValentinS83 <valentinstefan77@yahoo.com>
2024-04-01 18:53:47 +0300

    Question content

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 class Quiz {
-    constructor(questions){}
+    constructor(questions){
+        this.questions = questions;
+        this.questionContainer = document.getElementById('question');
+        this.answerButton = document.getElementById('answer-buttons');
+        this.feedback = document.getElementById('feedback');
+        this.nextButton = document.getElementById('next-button');
+        console.log(this.questions[0]);
+        this.showQuestion(this.questions[0]);
+
+    }
+
+    showQuestion(question) {
+        this.questionContainer.textContent = question.question;
+        this.answerButton.innerHTML = '';
+        for (const answer of question.answers) {
+            //const answearElement = `<li><button class="btn"></button></li>`;
+            const buttonContainer = document.createElement('li');
+            const buttonElement = document.createElement('button');
+            buttonElement.classList.add('btn');
+            buttonElement.textContent = answer.text;
+            this.answerButton.appendChild(buttonContainer);
+            buttonContainer.appendChild(buttonElement);
+            
+        }
+    }
 }
+// async folosim pe functia parinte
+async function initQuiz() {
+    // aducem datele din questions.json
+    const fileContent = await fetch('./questions.json');
+    // cu await afisam datale din questions.json
+    const quizQuestions = await fileContent.json();
+    console.log(quizQuestions);
+    const quiz = new Quiz(quizQuestions);
+}
+
+initQuiz();
+
